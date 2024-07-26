396076d65b99c92b064208af2318d992d17ee17d
Alyaa Mohammed <155733599+alyaamo20@users.noreply.github.com>
2024-07-26 06:40:57 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main ()
+{
+    int num1, num2  ,result ;
+
+    scanf ("%d \n %d" ,&num1,&num2);
+    printf("%d + %d = %d\n",num1 ,num2, result=num1+num2);
+    printf("%d - %d = %d\n",num1 ,num2, result=num1-num2);
+    printf("%d * %d = %d\n",num1 ,num2, result=num1*num2);
+    printf("%d / %d = %d\n",num1 ,num2, result=num1/num2);
+
+    return 0 ;
+
+}
